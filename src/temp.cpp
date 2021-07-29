f316507645edcca62ddaf7ba9dcd95f1eed6d0e3
wang <wangbaodong@meituan.com>
2021-07-29 14:34:27 +0800

    add temp

--- src/temp.cpp
+++ src/temp.cpp
@@ -13,7 +13,8 @@ int main()
     while(T--)
     {
         printf("------> Input data of m, n: ");
-        scanf("%d %d", &m, &n);
+        // scanf("%d %d", &m, &n);
+        cin >> m >> n;
         printf("======> Output data of m, n: ", m, n);
     }
     printf("--------------Finish--------------\n");
